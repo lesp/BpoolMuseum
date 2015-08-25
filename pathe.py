84c277460b1bfcc74b6ae2ba91a0ff446746103e
les Pounder <lespounder@gmail.com>
2015-08-25 13:43:32 +0000

    Updated and tested working

--- pathe.py
+++ pathe.py
@@ -8,6 +8,7 @@ pygame.mixer.init()
 def music(audio):
     pygame.mixer.music.load(audio)
     pygame.mixer.music.play(1)
+    time.sleep(5)
 
 def player(video):
     os.system("omxplayer -o local "+str(video))
@@ -17,37 +18,35 @@ def handle_touch(ch, evt):
     if ch == 0:
         print("Film 1")
         music("seaside.mp3")
-        time.sleep(3)
-        player("Pioneering.mp4")
-        time.sleep(0.2)
+        player("Blackpool_Illuminations.mp4")
     elif ch == 2:
         print("Film 2")
         music("seaside.mp3")
-        time.sleep(0.2)
+        player("Blackpool_Illuminations_Ride.mp4")
     elif ch == 4:
         print("Film 3")
         music("seaside.mp3")
-        time.sleep(0.2)
+        player("Blackpool_Issue_Title_Pathe_Pictorial_Goes_To_The_Seaside.mp4")
     elif ch == 5:
         print("Film 4")
         music("seaside.mp3")
-        time.sleep(0.2)
+        player("Blackpool_Promenade_1900.mp4")
     elif ch == 7:
         print("Film 5")
         music("seaside.mp3")
-        time.sleep(0.2)
+        player("Camera_Takes_Easter_Holiday_AKA_Blackpool_Beach.mp4")
     elif ch == 9:
         print("Film 6")
         music("seaside.mp3")
-        time.sleep(0.2)
+        player("Extra_!_Blackpool_Illuminations.mp4")
     elif ch == 11:
         print("Film 7")
         music("seaside.mp3")
-        time.sleep(0.2)
+        player("Holiday_News_Aka_Holiday_Scenes_-_Blackpool.mp4")
     elif ch == 12:
         print("Film 8")
         music("seaside.mp3")
-        time.sleep(0.2)
+        player("The_Pageant_Of_Progress.mp4")
     else:
         print("Press a key on the Wurlitzer")
 
